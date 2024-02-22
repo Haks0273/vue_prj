8d57e630e1b23a33ecf94629b9509198a1607e10
hawkism <13415931+hawkism@user.noreply.gitee.com>
2024-02-22 22:26:47 +0800

    layout的静态搭建

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+    import LayoutNav from '@/views/Layout/components/LayoutNav.vue'
+    import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
+    import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
+   
+</script>
+
+<template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
+
