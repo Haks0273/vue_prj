80d04c8c3d28cda94a5d2819b474db65bc473fe1
hawkism <13415931+hawkism@user.noreply.gitee.com>
2024-02-22 14:22:09 +0800

    axios_config

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { getCategory } from '@/apis/testAPI.js';
+
+getCategory().then(res =>{
+    console.log(res);
+});
 
 const app = createApp(App)
 
@@ -12,3 +17,4 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
---
+++ src/utils/http.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+// 创建一个axios实例
+const httpIntance = axios.create({
+    // 设置请求的baseURL
+    baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
+    // 设置请求的headers
+    headers: {
+        "Content-Type": "application/json"
+    },
+    // 设置请求的超时时间
+    timeout: 5000,
+});
+
+// 配置请求拦截器
+httpIntance.interceptors.request.use(
+    config => {
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+// 配置响应拦截器
+httpIntance.interceptors.response.use(
+    response => {
+        return response.data;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+export default httpIntance;
